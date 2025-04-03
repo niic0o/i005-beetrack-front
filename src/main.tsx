852ea2295d44e1284c5b6b7f0cbe8b134b925903
LaHuella <german.dosaula@gmail.com>
2025-04-03 11:43:07 +0200

    PrivateLayout merged with DashboardLayout(deprecated)

--- src/main.tsx
+++ src/main.tsx
@@ -4,27 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 import { Provider } from '@/components/ui/provider.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import DashboardPage from './pages/DashboardPage.tsx'
-import InventoryPage from './pages/InventoryPage.tsx'
-// import PrivateRoutes from './routes/PrivateRoutes.jsx'  No lo estoy usando por que sino me lleva a /login
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />, // Aqui va el PrivateRoutes
-    children: [
-      {
-        index: true,
-        element: <DashboardPage />
-      },
-      {
-        path: 'inventario',
-        element: <InventoryPage />
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
