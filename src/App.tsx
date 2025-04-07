2d3338520a9f81f6ec7a3c030c6ba8d3a8cb741b
Andres Taborda <andrestaborda43@gmail.com>
2025-04-07 17:44:45 +0200

    corrigiendo nombre archivo de test y configurando queryclientprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AppRoutes from './routes/AppRoutes'
 import '@/App.css'
 
 function App() {
-  
+  const queryClient = new QueryClient()
   return (
-    <AppRoutes />
+    <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+    </QueryClientProvider>
   )
 }
 
