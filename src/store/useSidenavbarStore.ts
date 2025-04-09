4a55d34610cbcdd069f5e3886a8d0db6fa050876
javierhidalgodev <javierhidalgodev@gmail.com>
2025-04-09 19:49:55 +0200

    ref: update to handle title topbar

--- src/store/useSidenavbarStore.ts
+++ src/store/useSidenavbarStore.ts
@@ -3,20 +3,33 @@ import { devtools } from 'zustand/middleware';
 
 interface SidenavbarState {
     isOpen: boolean;
-    setIsOpen: (status: boolean) => void;
+    isToggle: boolean;
+    titleToTopBar: string;
+    setIsOpen: (status? : boolean) => void;
+    setToggle: (status? : boolean) => void;
+    setTitleToTopBar: (title : string) => void;
 }
 
 const useSidenavbarStore = create<SidenavbarState>()(
     devtools(
         (set) => ({
             isOpen: false,
+            isToggle: false,
+            titleToTopBar: "Nombre de la tienda",
             setIsOpen: (status) => {
-
-                set({ isOpen: status })
-
-                status
-                    ? document.body.style.overflow = "hidden"
-                    : document.body.style.overflow = "auto";
+                set((state) => ({
+                    isOpen: status ? status : !state.isOpen
+                }))
+            },
+            setToggle: (status) => {
+                set((state) => ({
+                    isToggle: status ? status : !state.isToggle
+                }))
+            },
+            setTitleToTopBar: (title) => {
+                set({
+                    titleToTopBar: title
+                })
             },
         }),
 { name: "SidenavbarStore" }
