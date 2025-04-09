9ce5d4758716bc61a91ecd8e9dd94832d5d7fc99
javierhidalgodev <javierhidalgodev@gmail.com>
2025-04-09 19:53:23 +0200

    ref: update not found page && routing

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
-import PrivateLayout from '@/layout/PrivateLayout'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import DashboardPage from '@/pages/DashboardPage'
 import InventoryPage from '@/pages/InventoryPage'
 import PrivateRoutes from '@/routes/PrivateRoutes'
 import NotFoundPage from '@/pages/NotFoundPage'
+import ProfilePage from '@/pages/ProfilePage'
+import PrivateLayout from '@/layout/PrivateLayout'
 
 const AppRoutes = () => {
   return (
@@ -20,10 +21,12 @@ const AppRoutes = () => {
           <Route index element={<DashboardPage />} />
           <Route path="home" element={<DashboardPage />} />
           <Route path="inventario" element={<InventoryPage />} />
+          <Route path="perfil" element={<ProfilePage />} />
         </Route>
       </Route>
 
-      <Route path="*" element={<NotFoundPage />}></Route>
+      <Route path="/404" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate to={"/404"} replace />} />
     </Routes>
   )
 }
