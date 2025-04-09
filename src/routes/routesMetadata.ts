8e9d6d79e59fab06fe8a7ea086e345a2f9c934cd
javierhidalgodev <javierhidalgodev@gmail.com>
2025-04-09 19:24:51 +0200

    feat: metadata routes

---
+++ src/routes/routesMetadata.ts
@@ -0,0 +1,13 @@
+const ROUTES_METADATA: Record<string, { title: string }> = {
+	"/": { title: "Nombre tienda" }, // Cambiar por contenido del authStore
+	"/home": { title: "Dashboard" },
+	"/inventario": { title: "Inventario" },
+	"/ventas": { title: "Ventas" },
+	"/estadisticas": { title: "Estadísticas" },
+	"/perfil": { title: "Perfil" },
+	"/404": { title: "Página no encontrada" },
+	// "/login": { title: "Iniciar sesión" },
+	// "/register": { title: "Registro" },
+}
+
+export default ROUTES_METADATA
