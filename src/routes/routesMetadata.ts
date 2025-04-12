c3189bf3b765010b1d05503dbdaacc5002aeaa9a
javierhidalgodev <javierhidalgodev@gmail.com>
2025-04-12 19:01:59 +0200

    ref: update routes path

--- src/routes/routesMetadata.ts
+++ src/routes/routesMetadata.ts
@@ -1,10 +1,10 @@
 const ROUTES_METADATA: Record<string, { title: string }> = {
 	"/": { title: "Nombre tienda" }, // Cambiar por contenido del authStore
 	"/home": { title: "Dashboard" },
-	"/inventario": { title: "Inventario" },
-	"/ventas": { title: "Ventas" },
-	"/estadisticas": { title: "Estadísticas" },
-	"/perfil": { title: "Perfil" },
+	"/inventory": { title: "Inventario" },
+	"/sales": { title: "Ventas" },
+	"/stadistics": { title: "Estadísticas" },
+	"/profile": { title: "Perfil" },
 	"/404": { title: "Página no encontrada" },
 	// "/login": { title: "Iniciar sesión" },
 	// "/register": { title: "Registro" },
