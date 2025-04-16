ea61d679a5bab5b6038b939a3eb2e7606dc41465
javierhidalgodev <javierhidalgodev@gmail.com>
2025-04-16 19:16:52 +0200

    ref: update profile link to sidenavbar

--- src/components/Sidenavbar/SidenavbarComponents/ProfileDropDownMenu.tsx
+++ src/components/Sidenavbar/SidenavbarComponents/ProfileDropDownMenu.tsx
@@ -1,13 +1,19 @@
-import { Box, Button, Menu, Portal, Text } from "@chakra-ui/react";
+import { Box, Button, Menu, Portal, useBreakpointValue } from "@chakra-ui/react";
 import { useState } from "react";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp, MdLogout, MdPerson } from "react-icons/md";
 import NavItem from "./NavItem";
 import { FaCircleUser } from "react-icons/fa6";
 import { useLocation } from "react-router-dom";
+import { useLogout } from "@/hooks/useAuth";
+import useSidenavbarStore from "@/store/useSidenavbarStore";
 
 const ProfileDropDownMenu = () => {
     const [profileDropDownIsOpen, setProfileDropDownIsOpen] = useState<boolean>(false);
-    const  location = useLocation();
+    const { isToggle } = useSidenavbarStore();
+    const isMobile = useBreakpointValue({ base: true, sm: true, md: false })
+
+    const logout = useLogout();
+    const location = useLocation();
 
     return (
         <Menu.Root positioning={{ placement: "top-start", sameWidth: true }} variant={"solid"} onOpenChange={() => setProfileDropDownIsOpen(!profileDropDownIsOpen)} defaultOpen={false}>
@@ -18,18 +24,20 @@ const ProfileDropDownMenu = () => {
                     mt={"auto"}
                     p={"14px"}
                     h={"auto"}
-                    bg={location.pathname === "/profile" ? "colorPalette.solid" : "" }
+                    bg={location.pathname === "/profile" ? "colorPalette.solid" : ""}
                     justifyContent={"start"}
                     alignItems={"center"}
                     fontWeight={"bold"}
                     colorPalette={"navItem"}
                     rounded={"16px"}>
                     <MdPerson />
-                    Usuario
-                    <Box asChild ml={"auto"}>
-                        {profileDropDownIsOpen
-                            ? <MdKeyboardArrowDown />
-                            : <MdKeyboardArrowUp />}
+                    <Box w={"full"} display={{ base: "inherit", md: isToggle ? "none" : "" }}>
+                        Usuario
+                        <Box asChild ml={"auto"}>
+                            {profileDropDownIsOpen
+                                ? <MdKeyboardArrowDown />
+                                : <MdKeyboardArrowUp />}
+                        </Box>
                     </Box>
                 </Button>
             </Menu.Trigger>
@@ -37,10 +45,22 @@ const ProfileDropDownMenu = () => {
                 <Menu.Positioner>
                     <Menu.Content spaceY={2}>
                         <Menu.Item asChild value="profile-link" _highlighted={{ color: "black" }}>
-                            <NavItem to="/logout" icon={<MdLogout />} text="Logout" />
+                            {/* <NavItem to="/logout" icon={<MdLogout />} text="Logout" /> */}
+                            <Button
+                                p={"14px"}
+                                h={"auto"}
+                                variant={"ghost"}
+                                colorPalette={"navItem"}
+                                fontWeight={"bold"}
+                                rounded={"16px"}
+                                justifyContent={"start"}
+                                onClick={() => logout.mutate()}>
+                                    <MdLogout />
+                                Logout
+                            </Button>
                         </Menu.Item>
                         <Menu.Item asChild value="logout-link" _highlighted={{ color: "black" }}>
-                            <NavItem to="/profile" icon={<FaCircleUser />} text="Perfil" />
+                            <NavItem to="/profile" icon={<FaCircleUser />} text="Perfil" toggeable={false} />
                         </Menu.Item>
                     </Menu.Content>
                 </Menu.Positioner>
