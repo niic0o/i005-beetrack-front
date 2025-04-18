52b421b3f703ce3cef52c2519770a8d37a29071d
Jhoann G <jhoannegd@gmail.com>
2025-04-18 03:20:58 +0200

    modify-temp:./hooks-./pages-./store

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,53 +1,53 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
-import { User } from "@/types/authType";
-import { Query } from "@tanstack/react-query";
-import { QueryParams } from "@/types/utilsAppTypes";
-interface AppError {
-  message: string;
-  code?: string | number;
-}
+// import { create } from "zustand";
+// import { devtools } from "zustand/middleware";
+// import { User } from "@/types/authType";
+// import { Query } from "@tanstack/react-query";
+// import { QueryParams } from "@/types/utilsAppTypes";
+// interface AppError {
+//   message: string;
+//   code?: string | number;
+// }
 
-interface AuthState {
-  user: User | null;
-  loading: boolean;
-  isAuthenticated: boolean;
-  error: AppError | null;
-  queryParams: QueryParams
+// interface AuthState {
+//   user: User | null;
+//   loading: boolean;
+//   isAuthenticated: boolean;
+//   error: AppError | null;
+//   queryParams: QueryParams
 
-  setLoading: (isLoading: boolean) => void;
-  setUser: (user: User | null) => void;
-  setIsAuthenticated: (isAuthenticated: boolean) => void;
-  setError: (error: AppError | null) => void;
-  resetState: () => void;
-}
+//   setLoading: (isLoading: boolean) => void;
+//   setUser: (user: User | null) => void;
+//   setIsAuthenticated: (isAuthenticated: boolean) => void;
+//   setError: (error: AppError | null) => void;
+//   resetState: () => void;
+// }
 
-const initialState = {
-  user: null,
-  loading: false,
-  isAuthenticated: false,
-  error: null,
-  queryParams: { resource: "auth" },
-};
+// const initialState = {
+//   user: null,
+//   loading: false,
+//   isAuthenticated: false,
+//   error: null,
+//   queryParams: { resource: "auth" },
+// };
 
-const useAuthStore = create<AuthState>()(
-  devtools(
-    (set) => ({
-      ...initialState,
+// const useAuthStore = create<AuthState>()(
+//   devtools(
+//     (set) => ({
+//       ...initialState,
 
-      setLoading: (isLoading: boolean) => set({ loading: isLoading }),
+//       setLoading: (isLoading: boolean) => set({ loading: isLoading }),
 
-      setUser: (user: User | null) => set({ user }),
+//       setUser: (user: User | null) => set({ user }),
 
-      setIsAuthenticated: (isAuthenticated: boolean) =>
-        set({ isAuthenticated }),
+//       setIsAuthenticated: (isAuthenticated: boolean) =>
+//         set({ isAuthenticated }),
 
-      setError: (error: AppError | null) => set({ error }),
+//       setError: (error: AppError | null) => set({ error }),
 
-      resetState: () => set(initialState),
-    }),
-    { name: "AuthStore" }
-  )
-);
+//       resetState: () => set(initialState),
+//     }),
+//     { name: "AuthStore" }
+//   )
+// );
 
-export default useAuthStore;
+// export default useAuthStore;
