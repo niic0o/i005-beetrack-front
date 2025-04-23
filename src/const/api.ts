00445f322de4960403c3bff23e31059ab7697b0b
javierhidalgodev <javierhidalgodev@gmail.com>
2025-04-23 13:05:03 +0200

    fix: api reports url

--- src/const/api.ts
+++ src/const/api.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = 'http://localhost:3000/api';
 export const PRODUCTS_ENDPOINT = `${API_BASE_URL}/products`;
 export const SALES_ENDPOINT = `${API_BASE_URL}/sales`;
-export const REPORTS_ENDPOINT = `${API_BASE_URL}/reports`;
+export const REPORTS_ENDPOINT = `${API_BASE_URL}/dashboard`;
 export const AUTH_ENDPOINT = `${API_BASE_URL}/auth`;
 export const PROFILE_ENDPOINT = `${API_BASE_URL}/profile`;
