75f8d31c96d99b8753de432002f81f987dfdb008
javierhidalgodev <javierhidalgodev@gmail.com>
2025-05-13 18:00:24 +0200

    ref: update to work with env variables

--- src/const/api.ts
+++ src/const/api.ts
@@ -1,4 +1,7 @@
-export const API_BASE_URL = 'https://i005-beetrack-back.onrender.com/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
+
+console.log(import.meta.env.VITE_API_BASE_URL);
+
 export const PRODUCTS_ENDPOINT = `${API_BASE_URL}/products`;
 export const SALES_ENDPOINT = `${API_BASE_URL}/sales`;
 export const REPORTS_ENDPOINT = `${API_BASE_URL}/dashboard`;
